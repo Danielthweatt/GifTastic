e235fec27b28e2e11d94664d0515304db4296838
Danielthweatt <dthweatt192@yahoo.com>
2018-03-23 20:49:55 -0500

    worked on js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,12 @@
 
 // variables
 
-let topicArray = ['Captain America', 'Iron Man', 'Spider-Man', 'Thor', ];
+let topicArray = ['Captain America', 'Iron Man', 'Spider-Man', 'Thor', 'Hulk', 
+'Black Widow'];
+let character;
+let newTopic;
+let textInput = $('#newCharacter');
+let gifs = $('#gifs');
 
 // function to create buttons
 
@@ -10,36 +15,50 @@ function makeButtons() {
   $('#buttons').empty();
   for (let i = 0; i < topicArray.length; i++) {
     let button = $('<button>');
-    button.addClass('btn btn-info col-xs-3');
+    button.addClass('character btn btn-info col-xs-3');
+    button.attr('data-name', topicArray[i]);
     button.text(topicArray[i]);
     $('#buttons').append(button);
   };
 };
 
-makeButtons();
-
 // function to add a button
+
+$('#submitButton').on('click', function(event) {
+  event.preventDefault();
+  newTopic = textInput.val().trim();
+  topicArray.push(newTopic);
+  textInput.val('');
+  makeButtons();
+});
 
 // function to make API calls
 
-//example API call (works so far!)
-let queryURL = 'https://api.giphy.com/v1/gifs/search?q=spiderman&limit=10&rating=pg-13&api_key=JxYv2NDJkk8wyy4ZZsHzJFNu1UMdIZr8';
+$(document).on("click", ".character", function() {
+  character = $(this).attr('data-name');
+  let queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + character + '&limit=10&api_key=JxYv2NDJkk8wyy4ZZsHzJFNu1UMdIZr8';
+  console.log(queryURL);
+  gifs.empty();
 
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   }).then(function(response) {
-//       let gifStill;
-//       let gif;
-//       console.log(response);
-//       for (let i = 0; i < response.data.length; i++) {
-//         gifStill = response.data[i].images.original_still.url;
-//         gif = response.data[i].images.original.url;
-//         console.log(gifStill);
-//         console.log(gif);
-//         $('#gifs').append(`<img src="${gifStill}">`);
-//         // $('#gifs').append(`<img src="${gif}">`);
-//       };
-//   });
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+      let gifStill;
+      let gif;
+      console.log(response);
+      for (let i = 0; i < response.data.length; i++) {
+        gifStill = response.data[i].images.original_still.url;
+        gif = response.data[i].images.original.url;
+        gifrating = response.data[i].rating;
+        gifs.append(`<img src="${gifStill}">`);
+      };
+  });
 
-  // function to animate GIFS
+});
+
+// function to animate GIFS
+
+// function calls 
+
+makeButtons();
